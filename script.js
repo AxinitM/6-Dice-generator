ffeab881ceda17efcd74774e9ee9d2401aad4402
Andrei Martynenko <139020977+AxinitM@users.noreply.github.com>
2025-04-22 18:31:04 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const diceImg = document.getElementById("dice");
+const imagePath = "./images/";
+const diceFaces = 6;
+const changeInterval = 100;
+const totalDelay = 1500;
+const numberOfChanges = totalDelay / changeInterval;
+let intervalId;
+
+diceImg.addEventListener("click", () => {
+  let changeCounter = 0;
+  clearInterval(intervalId);
+  diceImg.style.border = "";
+
+  intervalId = setInterval(() => {
+    const tempRandomNumber = Math.floor(Math.random() * diceFaces) + 1;
+    diceImg.src = `${imagePath}dice-${tempRandomNumber}.png`;
+    changeCounter++;
+
+    if (changeCounter >= numberOfChanges) {
+      clearInterval(intervalId);
+      const finalRandomNumber = Math.floor(Math.random() * diceFaces) + 1;
+      diceImg.src = `${imagePath}dice-${finalRandomNumber}.png`;
+      diceImg.style.border = "3px solid var(--resultDiceBorder)";
+    }
+  }, changeInterval);
+});
